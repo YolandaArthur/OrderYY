abc46660f996b5a6be9d6ca675d01addbeb61ccd
[Yves Alexandre] <[yves.alexandre@gmail.com]>
2022-12-13 10:51:59 +0100

    added order and sandwich classes

---
+++ sandwich/src/model/Order.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Order {
+    private Sandwich sandwich;
+    private LocalDate date;
+    private String personName;
+    private String course;
+    private String typeBread;
+    private Boolean withRawVegetables;
+    private String comment;
+
+    public boolean validate(){
+        return true;
+    }
+
+}
---
+++ sandwich/src/model/Sandwich.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Sandwich {
+    private String SandwichName;
+}
