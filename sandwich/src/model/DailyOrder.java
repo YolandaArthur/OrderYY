705b96140e331bd2056bc11f0aa2e28fb907dde2
YolandaArthur <yolanda_gc@hotmail.com>
2022-12-13 11:22:49 +0100

    add orderlist

---
+++ sandwich/src/model/DailyOrder.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.List;
+
+public class DailyOrder {
+    private List<Order> ordersList;
+
+    public void addOrder(){
+        // validation :  max 2  sandwiches
+        // save new order in repo
+
+    }
+
+    public void printOrder(){
+
+    }
+
+}
